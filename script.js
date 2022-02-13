f3bc908f5a58f1853d28dba5774d4b1edb100663
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-13 22:00:34 +0100

    all the names are found as objects and toLowerCase/toUpperCase

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ async function getJson() {
   students = await data.json();
 
   console.table(students);
-  prepareObjects();
+  createStudents();
 }
 
-function prepareObjects() {
+function createStudents() {
   students.forEach((object) => {
     const Student = {
       firstName: "",
@@ -24,6 +24,50 @@ function prepareObjects() {
       image: "",
       house: "",
     };
+
+    // create a objects from a prototype
     const studentInfo = Object.create(Student);
+
+    // ----- First name -----
+    // Trim objects for whitespace
+    let originalName = object.fullname.trim();
+    //Find first name in string
+    if (originalName.includes(" ")) {
+      studentInfo.firstName = originalName.substring(originalName.indexOf(0), originalName.indexOf(" "));
+    } else {
+      studentInfo.firstName = originalName.substring(originalName.indexOf(0));
+    }
+    studentInfo.firstName = studentInfo.firstName.substring(0, 1).toUpperCase() + studentInfo.firstName.substring(1).toLowerCase();
+
+    //----- Middle name -----
+    //Find the middle name (if any)
+    studentInfo.middleName = originalName.substring(originalName.indexOf(" ") + 1, originalName.lastIndexOf(" "));
+    //Change to upper- and lower case
+    studentInfo.middleName = studentInfo.middleName.substring(0, 1).toUpperCase() + studentInfo.middleName.substring(1).toLowerCase();
+
+    //----- Nick name -----
+    //Find the nick name (if any)
+    if (originalName.includes('"')) {
+      studentInfo.middleName = undefined;
+      studentInfo.nickName = originalName.substring(originalName.indexOf('"') + 1, originalName.lastIndexOf('"'));
+    }
+
+    // ----- Last name -----
+    //Find last name in string
+    if (originalName.includes(" ")) {
+      studentInfo.lastName = originalName.substring(originalName.lastIndexOf(" ") + 1);
+      //Change to upper- and lower case
+      studentInfo.lastName = studentInfo.lastName.substring(0, 1).toUpperCase() + studentInfo.lastName.substring(1).toLowerCase();
+    }
+
+    // ----- House -----
+    //Trim objects
+    let originalHouse = object.house.trim();
+    //Find the name of the house
+    studentInfo.house = originalHouse;
+    //Change to upper- and lower case
+    studentInfo.house = studentInfo.house.substring(0, 1).toUpperCase() + studentInfo.house.substring(1).toLowerCase();
+
+    console.table(studentInfo);
   });
 }
